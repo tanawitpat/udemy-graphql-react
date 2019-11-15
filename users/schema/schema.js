c70e8b7cf36694ddeb35afefb01d31ec6a4938d7
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-11-15 15:03:31 +0700

    Section 4, Lecture 18

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql')
-const _ = require('lodash')
+const axios = require('axios')
 
 const {
     GraphQLObjectType,
@@ -8,17 +8,28 @@ const {
     GraphQLSchema
 } = graphql
 
-const users = [
-    { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Samantha', age: 21}
-]
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+})
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(res => res.data)
+            }
+        }
     }
 })
 
@@ -29,8 +40,9 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id })
-            }    
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(res => res.data)
+            }
         }
     }
 })
