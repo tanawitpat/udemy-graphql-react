876bba90345490caef77581e1d3bfd694bb3e261
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-11-19 14:55:40 +0700

    Section 11, Lecture 71

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class LyricList extends Component {
-    renderLyrics() {
-        return this.props.lyrics.map(({ id, content }) => {
-            return (
-                <li key={id}>{content}</li>
-            )
-        })
-    }
-    render() {
-        return (
-            <ul className="collection">
-                {this.renderLyrics()}
-            </ul>
-        )
-    }
+  onLike(id) {
+    this.props.mutate({ variables: { id } })
+  }
+
+  renderLyrics() {
+    return this.props.lyrics.map(({ id, content, likes }) => {
+      return (
+        <li key={id} className="collection-item">
+          {content}
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
+        </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <ul className="collection">
+        {this.renderLyrics()}
+      </ul>
+    )
+  }
 }
 
-export default LyricList
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`
+
+export default graphql(mutation)(LyricList)
